20499b7a01cb90b765be9ca78c53c22d50015090
Vladislav V <0428vlad0428@mail.ru>
2021-02-19 09:20:39 +0300

    8kyu

--- 8_kyu/task-1.js
+++ 8_kyu/task-1.js
@@ -1,1 +1,15 @@
-console.log('hi')
+// Given an array of integers, return a new array with each value doubled.
+//
+//     For example:
+//
+//     [1, 2, 3] --> [2, 4, 6]
+//
+// For the beginner, try to use the map method - it comes in very handy quite a lot so is a good one to know.
+
+let x = [1,2,3]
+
+let result = x.map( number =>{
+    return(number*2)
+})
+
+console.log(result)
