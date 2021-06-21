83953214d6d4433860a0c296e3b22e0218640b61
Vladislav V <0428vlad0428@mail.ru>
2021-06-21 13:37:22 +0300

    6kyu :)

--- 6_kyu/main6kyu.js
+++ 6_kyu/main6kyu.js
@@ -117,6 +117,38 @@ clear()
     }
 
     digital_root(493193234343343433) //7
+    clear()
+}
+{
+    // ----------6-kyu----------6-kyu----------6-kyu----------6-kyu----------6-kyu----------
+
+    // Write a function that takes in a string of one or more words, and returns the same string, but with all five or more letter words reversed (like the name of this kata).
+    //
+    // Strings passed in will consist of only letters and spaces.
+    // Spaces will be included only when more than one word is present.
+    // Examples:
+    //
+    // spinWords("Hey fellow warriors") => "Hey wollef sroirraw"
+    // spinWords("This is a test") => "This is a test"
+    // spinWords("This is another test") => "This is rehtona test"
+    function spinWords(str) {
+        let arr = str.split(' ')
+        let newArr = []
+        let result
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i].length >= 5) {
+                result = arr[i].split('').reverse().join('')
+                newArr.push(result)
+            } else {
+                newArr.push(arr[i])
+            }
+        }
+        let finalStr
+
+        console.log(newArr.join(' '))
+    }
+
+    spinWords("Welcome")
 }
 
 
