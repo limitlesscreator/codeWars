06ab55e4f661d37eed5fd3bd3bbeaf4fa3a4a94a
Vladislav V <0428vlad0428@mail.ru>
2021-06-21 14:18:06 +0300

    6kyu :)

--- 6_kyu/main6kyu.js
+++ 6_kyu/main6kyu.js
@@ -150,5 +150,37 @@ clear()
 
     spinWords("Welcome")
 }
+{
+    // ----------6-kyu----------6-kyu----------6-kyu----------6-kyu----------6-kyu----------
+
+    // You probably know the "like" system from Facebook and other pages. People can "like" blog posts, pictures or other items. We want to create the text that should be displayed next to such an item.
+    //
+    // Implement a function likes :: [String] -> String, which must take in input array, containing the names of people who like an item. It must return the display text as shown in the examples:
+    //
+    // likes [] -- must be "no one likes this"
+    // likes ["Peter"] -- must be "Peter likes this"
+    // likes ["Jacob", "Alex"] -- must be "Jacob and Alex like this"
+    // likes ["Max", "John", "Mark"] -- must be "Max, John and Mark like this"
+    // likes ["Alex", "Jacob", "Mark", "Max"] -- must be "Alex, Jacob and 2 others like this"
+    // For 4 or more names, the number in and 2 others simply increases.
+    clear()
+
+    function likes(names) {
+        let arr = names
+        if (arr.length === 0) {
+            console.log('no one likes this')
+        } else if (arr.length === 1) {
+            console.log(`${arr[0]} likes this`)
+        } else if (arr.length === 2) {
+            console.log(`${arr[0]} and ${arr[1]} like this`)
+        } else if (arr.length === 3) {
+            console.log(`${arr[0]}, ${arr[1]} and ${arr[2]} like this`)
+        } else if (arr.length > 3) {
+            console.log(`${arr[0]}, ${arr[1]} and ${arr.length - 2} others like this`)
+        }
+    }
+
+    likes(['Peter'])
+}
 
 
