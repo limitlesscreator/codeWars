8b5cc43695eea2cdafb3d62ef1af1074aafc88ca
Vladislav V <0428vlad0428@mail.ru>
2021-06-26 15:32:25 +0300

    6kyu must to continue ...

--- 6_kyu/main6kyu.js
+++ 6_kyu/main6kyu.js
@@ -182,5 +182,55 @@ clear()
 
     likes(['Peter'])
 }
+{
+    // ----------6-kyu----------6-kyu----------6-kyu----------6-kyu----------6-kyu----------
+    // Count the number of Duplicates
+    // Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that occur more than once in the input string. The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits.
+    //
+    // Example
+    // "abcde" -> 0 # no characters repeats more than once
+    // "aabbcde" -> 2 # 'a' and 'b'
+    // "aabBcde" -> 2 # 'a' occurs twice and 'b' twice (`b` and `B`)
+    // "indivisibility" -> 1 # 'i' occurs six times
+    // "Indivisibilities" -> 2 # 'i' occurs seven times and 's' occurs twice
+    // "aA11" -> 2 # 'a' and '1'
+    // "ABBA" -> 2 # 'A' and 'B' each occur twice
+    clear()
+
+    function duplicateCount(text) {
+        let arr = text.toLowerCase().split('')
+        result = 0
+        points = 0
+       for (let i = 0; i < arr.length; i++){
+           if (points === 0){
+               for (let j = 0; j < arr.length; j++){
+
+               }
+           }
+       }
+
+        console.log(result - 1)
+    }
+    duplicateCount('aabbcde') // 2
+}
 
 
+// function duplicateCount(text) {
+//     let arr = text.toLowerCase().split('')
+//     result = 0
+//     points = 0
+//     let checkLetter = []
+//     for (let i = 0; i < arr.length; i++){
+//         for (let j = 0; j < arr.length; j++){
+//             if (arr[i] === arr[j] && points < 1 && checkLetter[j] !== arr[i]){
+//                 result++
+//                 points++
+//                 checkLetter.push(arr[i])
+//                 break
+//             }
+//             else continue;
+//         }points = 0
+//     }
+//
+//     console.log(result - 1)
+// }
