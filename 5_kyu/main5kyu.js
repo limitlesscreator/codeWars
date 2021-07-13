2d7e3238221213e792c3b1107832e003ad967f88
Vladislav V <0428vlad0428@mail.ru>
2021-07-13 17:45:43 +0300

    5kyu))

--- 5_kyu/main5kyu.js
+++ 5_kyu/main5kyu.js
@@ -56,3 +56,39 @@ const moveZeros = (arr) => {
 
 moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0]
 // i think it's easier than 5kyu, but it's 5 kyu))
+
+console.clear()
+// ----------5-kyu----------5-kyu----------5-kyu----------5-kyu----------5-kyu----------
+// omplete the function scramble(str1, str2) that returns true if a portion of str1 characters can be rearranged to match str2, otherwise returns false.
+//
+//     Notes:
+//
+// Only lower case letters will be used (a-z). No punctuation or digits will be included.
+//     Performance needs to be considered
+// Input strings s1 and s2 are null terminated.
+//     Examples
+// scramble('rkqodlw', 'world') ==> True
+// scramble('cedewaraaossoqqyt', 'codewars') ==> True
+// scramble('katas', 'steak') ==> False
+function scramble(str1, str2) {
+    let randomLetters = str1.split('')
+    let checkWord = str2.split('')
+    let points = 0
+    for (let i = 0; i < checkWord.length; i++){
+        if (points === checkWord.length){return}
+        else {
+            for (let j = 0; j < randomLetters.length; j++){
+                if (checkWord[i] === randomLetters[j]){
+                    points++
+                    break;
+                }
+            }
+        }
+    }
+    if (points === checkWord.length){
+        console.log(true)
+    }
+    else console.log(false)
+}
+scramble('rkqodlw','world')
+// finished
